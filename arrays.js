221409cc7a6978e2253f89600124fee845f3f19b
Raja H <rajahamid12.7@gmail.com>
2018-02-19 16:27:02 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,10 @@
+/* TODO: In arrays.js, define a variable called chocolateBars. 
+Its value should be an array of the strings snickers, hundred grand, kitkat, and skittles.
+*/
+
+var chocolateBars = {
+  "snickers",
+  "hundred grand",
+  "kitkat",
+  "skittles"
+}

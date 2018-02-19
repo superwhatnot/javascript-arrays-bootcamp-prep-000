4494d48061bde77eebfbba321885bd9b97f25eb3
Raja H <rajahamid12.7@gmail.com>
2018-02-19 16:32:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -24,3 +24,8 @@ return a new array and not modify the original.
 function addElementToBeginningOfArray(array, element){
   return [element, ...array]
 }
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+}
